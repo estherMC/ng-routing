6a42689464107de04c61c99f9c9f71d90d061b44
Esther Martorell <esther.martorell@allianz.com>
2023-07-11 14:48:27 +0200

    :sparkles: add canActivate to protect routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { ServersComponent } from "./servers/servers.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { AuthGuard } from "./auth-guard.service";
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -14,7 +15,7 @@ const appRoutes: Routes = [
     { path: ':id/:name', component: UserComponent },
   ] },
   //{ path: '', redirectTo: '/somewhere-else', pathMatch: 'full' } --> pathMatch = full revisa tota la ruta
-  { path: 'servers', component: ServersComponent, children: [
+  { path: 'servers', canActivate: [AuthGuard], component: ServersComponent, children: [
     { path: ':id', component: ServerComponent },
     { path: ':id/edit', component: EditServerComponent }
   ]},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
 
 // const appRoutes: Routes = [
 //   { path: '', component: HomeComponent },
@@ -46,7 +48,7 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule
     // RouterModule.forRoot(appRoutes)
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
